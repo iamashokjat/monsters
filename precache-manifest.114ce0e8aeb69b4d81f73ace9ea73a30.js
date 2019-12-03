6829dcfc9299c7014f4e1b7637f6238c86349a7c
iamashokjat <ashokjakad@gmail.com>
2019-12-03 16:03:59 +0530

    Updates

---
+++ precache-manifest.114ce0e8aeb69b4d81f73ace9ea73a30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7dc4c337cb747d2c0ab0fac44b07a54",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "27b7946126eb481b5fdd",
+    "url": "/monsters/static/css/main.897c06b1.chunk.css"
+  },
+  {
+    "revision": "d935b8e3403269409eb0",
+    "url": "/monsters/static/js/2.722cd26d.chunk.js"
+  },
+  {
+    "revision": "27b7946126eb481b5fdd",
+    "url": "/monsters/static/js/main.682f512a.chunk.js"
+  },
+  {
+    "revision": "a780c5024727f4e9d1d4",
+    "url": "/monsters/static/js/runtime-main.4488f325.js"
+  }
+]);
